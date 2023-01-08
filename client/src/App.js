92120a3c981f5e95a9ef0c4959c7baccdc7f688a
Doheera-kosi <kupourevans99@gmail.com>
2023-01-08 07:04:23 +0000

    Setup Redux, Redux-ToolKit & MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import { CssBaseline, ThemeProvider,  } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
 
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
+  // Tapping into the state we created
+  // and making it accessible everywhere
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  return <div className="app">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  </div>
 }
 
 export default App;
